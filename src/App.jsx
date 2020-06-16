c6c02291b36929666c26ee49ed48413b5dfb6cb4
NewbieInside <shabanov.konstantin@yahoo.com>
2020-06-16 16:42:51 +0300

    added song title component & redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import SongList from './components/SongList/SongList';
+import SongDetail from './components/SongDetail/SongDetail';
 
 import { selectSong } from './actions';
 
-const App = () =>{
+import styles from './App.module.css';
+
+const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className={styles.appContainer}>
+      <div className={styles.songListCont}>
+        <SongList />
+      </div>
+      <div className={styles.songDetailCont}>
+        <SongDetail />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
-
-
